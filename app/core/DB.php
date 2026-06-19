d6ec50d4cbe4b1342f46dcbce3a1e0739c4bcc0e
Gia Huy <nguyengiahuy141205@gmail.com>
2026-06-19 17:44:46 +0700

    feat<sinhvien,lophoc> : choose pageSize for paging

--- app/core/DB.php
+++ app/core/DB.php
@@ -1,7 +1,7 @@
 <?php
     class ConnectDB {
         private $host = "localhost";
-        private $port = "3306";
+        private $port = "3307";
         private $username = "root";
         private $password = "";
         private $database = "68pm34";
@@ -10,7 +10,7 @@
         public function connect() {
             $this->conn = null;
             try {
-                $this->conn = new PDO("mysql:host=" . $this -> host . ";port=" . $this -> port . ";dbname=" . $this -> database, $this -> username, $this -> password);
+                $this->conn = new PDO("mysql:host=" . $this -> host . ";port=" . $this -> port . ";dbname=" . $this -> database . ";charset=utf8mb4", $this -> username, $this -> password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $exception) {
                 echo "Connection error: " . $exception->getMessage();
